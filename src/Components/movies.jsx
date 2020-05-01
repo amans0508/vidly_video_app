9d140f7a804381676a06a2d18b21af699b3e3db2
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-05-01 20:45:30 +0530

    added heart icon

--- src/Components/movies.jsx
+++ src/Components/movies.jsx
@@ -35,6 +35,7 @@ class Movies extends Component {
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
+                  <i class="fa fa-heart-o" aria-hidden="true"></i>
                   <button
                     type="button"
                     className="btn btn-danger"
